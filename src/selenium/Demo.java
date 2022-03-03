a1a14636f9cd49a92967f40866ffa50a57bfcfee
Admin <Admin@DESKTOP-8SGU2MI>
2022-03-03 17:51:15 +0530

    4commit

--- src/selenium/Demo.java
+++ src/selenium/Demo.java
@@ -3,7 +3,6 @@ package selenium;
 public class Demo {public static void main(String[] args) {
 	System.out.println("Hii");
 	//   Hz from local
-	System.out.println("hi");
 	System.out.println("etghrwhr3");
 }
 
